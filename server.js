e57026734e9891f00e8e18fe52d1a8c38fe6ea81
solofervqz <solofervazquez@gmail.com>
2023-11-26 01:11:33 -0600

    Crear, Index, new, Libros

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views') //de donde viene el views
@@ -28,5 +29,6 @@ db.once('open', error => console.log('Conectado a Mongoose'))
 //ROUTES
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
